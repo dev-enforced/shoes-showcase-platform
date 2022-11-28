98ca2e00e6b7d9d131f9c051cd9fc4cfa0dfe839
Roshan J <devenforced@gmail.com>
2022-11-29 00:32:25 +0530

    [fix]: key alloted to stars

--- src/utilities/generateRatings.js
+++ src/utilities/generateRatings.js
@@ -4,9 +4,9 @@ const generateRatings = (ratingsGiven) => {
   const ratings = [1, 2, 3, 4, 5];
   const generatedRatings = ratings.map((everyRating) =>
     everyRating <= ratingsGiven ? (
-      <Filled size={15} color="#FFC94D" />
+      <Filled key={everyRating} size={15} color="#FFC94D" />
     ) : (
-      <Empty size={15} color="#FFC94D" />
+      <Empty key={everyRating} size={15} color="#FFC94D" />
     )
   );
   return generatedRatings;
